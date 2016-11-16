3d383dbde495d5f8a6b8d8a6c498b9c7843589f4
Antonio Fortino <thoniorf@gmail.com>
2016-11-16 22:56:44 +0100

    Added notes

--- calvtk_sciddicaT-unsafe/source/vtk.h
+++ calvtk_sciddicaT-unsafe/source/vtk.h
@@ -2,6 +2,12 @@
 #define VTK_H
 #include <vtkSmartPointer.h>
 #include <vtkDataSet.h>
+// ImageData because the sciddicaT's cellular space is 2-dimensional matrix like
+#include <vtkImageData.h>
+// LookupTable for topological value ( gray gradient) and debris value ( red gradient)
+#include<vtkLookupTable.h>
+
 #define VTK_SP(type,name) vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
 
 #endif
+// update the scalar value of cell and render
